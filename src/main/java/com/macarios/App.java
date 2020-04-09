8788b432a8b3743b4463cd55702ecae5e4aca483
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-04-09 13:27:05 +0200

    Initial commit

---
+++ src/main/java/com/macarios/App.java
@@ -0,0 +1,13 @@
+package com.macarios;
+
+/**
+ * @author Victor Macarios
+ * Data Structure course by Loaine Groner
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
